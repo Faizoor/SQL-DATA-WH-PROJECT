1e6c1c46717e0c13c199ead4f1470a6b99aa2e09
Faizoor <125073831+Faizoor@users.noreply.github.com>
2025-09-11 16:41:12 +0530

    Update init_database.sql

--- Scripts/init_database.sql
+++ Scripts/init_database.sql
@@ -1,1 +1,28 @@
+/*
+
+This script creates a new database named "datawarehouse" and within the database it will create theree schemas Bronze, silver and gold.
+
+PS: if the database named "datawarehouse" already exists please drop and run the script 
+
+*/
+
+--Use System Database "Master"
+use master ; 
+Go
+
+--Create Databse "datawarehouse"
+create database datawarehouse ;
+Go
+use datawarehouse;
+
+--Create Schemas
+create schema bronze;
+Go
+  
+create schema silver;
+Go
+
+create schema gold;
+Go
+
 
